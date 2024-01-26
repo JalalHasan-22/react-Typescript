da31085bbf818d7350186c66dd04f401594b6a98
JalalHasan-22 <jhasanj8@gmail.com>
2024-01-26 20:55:13 +0000

    lesson 5, state, events, and methods as props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import PokemonRow from './components/Pokemon-Row/Pokemon-row';
 import Pokemon from './pokemon.json';
+import PokemonInfo from './components/Pokemon-Info/PokemonInfo';
 
 function App() {
+  const [filter, setFilter] = useState('');
+  const [selectedItem, setSelectedItem] = useState('');
   return (
     <div
       style={{
@@ -12,19 +16,45 @@ function App() {
       }}
     >
       <h1 className='title'>Pokemon Search</h1>
-      <table width='100%'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Pokemon.slice(0, 20).map((pokemon) => {
-            return <PokemonRow pokemon={pokemon} key={pokemon?.id} />;
-          })}
-        </tbody>
-      </table>
+      <input
+        type='text'
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '70% 30%',
+          gridGap: '1rem',
+        }}
+      >
+        <table width='100%'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Pokemon.filter((pokemon) =>
+              pokemon.name.english
+                .toLocaleLowerCase()
+                .includes(filter.toLocaleLowerCase()),
+            )
+              .slice(0, 20)
+              .map((pokemon) => {
+                return (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={pokemon?.id}
+                    onSelect={(pokemon) => setSelectedItem(pokemon)}
+                  />
+                );
+              })}
+          </tbody>
+        </table>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </div>
     </div>
   );
 }
--- src/components/Pokemon-Row/Pokemon-row.js
+++ src/components/Pokemon-Row/Pokemon-row.js
@@ -1,8 +1,11 @@
-function PokemonRow({ pokemon }) {
+function PokemonRow({ pokemon, onSelect }) {
   return (
     <tr>
       <td>{pokemon?.name?.english}</td>
       <td>{pokemon?.type.join(', ')}</td>
+      <td>
+        <button onClick={() => onSelect(pokemon)}>Select</button>
+      </td>
     </tr>
   );
 }
